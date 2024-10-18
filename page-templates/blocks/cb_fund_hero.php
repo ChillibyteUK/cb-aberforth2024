5691dc99637254db9d3642fffe6e593976a14cba
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-10-18 11:53:13 +0100

    Update cb_fund_hero.php

--- page-templates/blocks/cb_fund_hero.php
+++ page-templates/blocks/cb_fund_hero.php
@@ -1,11 +1,11 @@
-<section class="fund_hero">
+<section class="fund_hero pb-5">
     <div class="container-xl">
-        <div class="row">
-            <div class="col-md-9">
+        <div class="row g-4">
+            <div class="col-md-8">
                 <h1><?=get_field('title')?></h1>
                 <a href="#" class="button">SHEET</a>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4 d-flex gap-4 justify-content-end">
                 <?php
                 // Define the XML URL
                 $url = get_field('ticker') ?? null;
@@ -22,21 +22,33 @@
                     die("Error: Failed to parse XML.");
                 }
 
-                // Convert XML to JSON and then to an associative array for easy access
-                $json = json_encode($xml);
-                $data = json_decode($json, true);
+                $shares = isset($xml->share) ? $xml->share : [$xml];
 
-                $symbol = $data['Symbol'];
-                $currentPrice = $data['CurrentPrice'];
-                $change = $data['Change'];
-                $date = $data['Date'];
+                // Display the data for each share
+                foreach ($shares as $share) {
+                    // Convert XML to JSON and then to an associative array for easy access
+                    $json = json_encode($share);
+                    $data = json_decode($json, true);
+
+                    if ($data) {
+                        $symbol = $data['Symbol'];
+                        $currentPrice = $data['CurrentPrice'];
+                        $change = $data['Change'];
+                        $date = $data['Date'];
+                        ?>
+                    <div class="ticker">
+                        <div class="ticker__date"><?=$date?></div>
+                        <div class="ticker__symbol"><?=$symbol?></div>
+                        <div class="ticker__price"><?=$currentPrice?></div>
+                        <div class="ticker__change ticker__change--up"><?=$change?></div>
+                    </div>
+                        <?php
+                    }
+                    else {
+                        echo 'Error: No data found.';
+                    }
+                }
                 ?>
-                <div class="ticker">
-                    <div class="ticker__date"><?=$date?></div>
-                    <div class="ticker__symbol"><?=$symbol?></div>
-                    <div class="ticker__price"><?=$currentPrice?></div>
-                    <div class="ticker__change ticker__change--up"><?=$change?></div>
-                </div>
             </div>
         </div>
     </div>
