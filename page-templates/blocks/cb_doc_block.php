62cc5e964bf83c619ee96d688494addc1de39500
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-10-24 10:58:17 +0100

    doc block

--- page-templates/blocks/cb_doc_block.php
+++ page-templates/blocks/cb_doc_block.php
@@ -3,19 +3,23 @@
         <table class="table fs-300">
             <tbody>
                 <?php
-                foreach (get_field('files') as $f) {
-                    $attachment_url = wp_get_attachment_url($f);
-                    $file_path = get_attached_file($f);
-                    $file_size = filesize($file_path);
-                    ?>
-                    <tr onclick="window.location.href='<?=$attachment_url?>'" style="cursor: pointer;">
-                        <td class="fw-500"><?=get_the_title($f)?></td>
-                        <td><?=esc_html(get_the_terms($f, 'doccat')[0]->name ?? '')?></td>
-                        <td><?=formatBytes($file_size,0)?></td>
-                        <td><?=get_the_date('d M Y',$f); ?></td>
-                        <td><a href="<?=$attachment_url?>" download class="icon-download" style="text-decoration: none; color: inherit;"></a></td>
-                    </tr>
-                <?php
+                $docs = get_field('files') ?? null;
+                if ($docs) {
+                    foreach (get_field('files') as $f) {
+                        $file = get_field('file',$f);
+                        $attachment_url = wp_get_attachment_url($file);
+                        $file_path = get_attached_file($file);
+                        $file_size = filesize($file_path);
+                        ?>
+                <tr onclick="window.location.href='<?=$attachment_url?>'" style="cursor: pointer;">
+                    <td class="fw-500"><?=get_the_title($f)?></td>
+                    <td><?=esc_html(get_the_terms($f, 'doccat')[0]->name ?? '')?></td>
+                    <td><?=formatBytes($file_size,0)?></td>
+                    <td><?=get_the_date('d M Y',$f); ?></td>
+                    <td><a href="<?=$attachment_url?>" download class="icon-download" style="text-decoration: none; color: inherit;"></a></td>
+                </tr>
+                    <?php
+                    }
                 }
                 ?>
             </tbody>
