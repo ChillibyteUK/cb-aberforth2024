bcc856775efae8d7c426211f795b95cdf5c37d81
LamcatUK <dan@chillibyte.co.uk>
2024-11-01 15:38:53 +0000

    Update search.php

--- search.php
+++ search.php
@@ -42,8 +42,7 @@ $theme = 'default';
 					<div class="border-bottom pb-3 mb-2">
 						<a href="<?php echo $attachment_url; ?>" download>
 							<div class="fs-300"><?= esc_html(get_the_terms(get_the_ID(), 'doccat')[0]->name ?? ''); ?></div>
-							<div class="fs-300"><?php echo esc_html(get_the_terms(get_the_ID(), 'doctype')[0]->name ?? ''); ?></div>
-							<div class="d-flex justify-content-between">
+							<div class="d-flex w-100 justify-content-between">
 								<h2 class="h4 pt-3"><?php the_title() ?></h2>
 								<div class="icon-download"></div>
 							</div>
