925993d37a7d4bd512f1afb3d0ca1af67d8d1bf3
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-07 15:16:47 +0000

    form bits

--- page-templates/blocks/cb_gravity_form.php
+++ page-templates/blocks/cb_gravity_form.php
@@ -1,5 +1,5 @@
 <section class="gravityform">
-    <div class="container-xl">
+    <div class="container-xl mx-auto">
         <?php
         if (get_field('form_id') ?? null) {
             echo do_shortcode('[gravityform id="' . get_field('form_id') . '" title="false"]');
