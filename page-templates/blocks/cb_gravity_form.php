5862844c2b20a508ab9c038d35f9a0d2ae1a1ece
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-07 14:31:11 +0000

    gf

---
+++ page-templates/blocks/cb_gravity_form.php
@@ -0,0 +1,9 @@
+<section class="gravityform">
+    <div class="container-xl">
+        <?php
+        if (get_field('form_id') ?? null) {
+            echo do_shortcode('[gravityform id="' . get_field('form_id') . '" title="false"]';
+        }
+        ?>
+    </div>
+</section>
