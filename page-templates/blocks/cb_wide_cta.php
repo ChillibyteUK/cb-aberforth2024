3e7166b8d0cd3f647c32dda818aa0495e4252946
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-11 11:59:15 +0000

    wide cta

--- page-templates/blocks/cb_wide_cta.php
+++ page-templates/blocks/cb_wide_cta.php
@@ -1,10 +1,10 @@
 <?php
-$l = get_field('link');
+$l = get_field('cta_link','option');
 ?>
 <section class="wide_cta py-5">
     <div class="container-xl py-4 text-center">
-        <h2><?=get_field('title')?></h2>
-        <div class="w-constrained mx-auto pb-4"><?=get_field('content')?></div>
+        <h2><?=get_field('cta_title','option')?></h2>
+        <div class="w-constrained mx-auto pb-4"><?=get_field('cta_content','option')?></div>
         <a href="<?=$l['url']?>" target="<?=$l['target']?>" class="button"><?=$l['title']?></a>
     </div>
 </section>
