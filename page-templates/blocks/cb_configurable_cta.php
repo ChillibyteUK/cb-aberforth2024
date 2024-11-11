213a6a6c2cb09498d0a66554b9ec46ae1484c2cd
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-11 13:04:43 +0000

    Update cb_configurable_cta.php

--- page-templates/blocks/cb_configurable_cta.php
+++ page-templates/blocks/cb_configurable_cta.php
@@ -1,10 +1,10 @@
 <?php
-$l = get_field('cta_link');
+$l = get_field('link');
 ?>
 <section class="wide_cta py-5">
     <div class="container-xl py-4 text-center">
-        <h2><?=get_field('cta_title')?></h2>
-        <div class="w-constrained mx-auto pb-4"><?=get_field('cta_content')?></div>
+        <h2><?=get_field('title')?></h2>
+        <div class="w-constrained mx-auto pb-4"><?=get_field('content')?></div>
         <a href="<?=$l['url']?>" target="<?=$l['target']?>" class="button"><?=$l['title']?></a>
     </div>
 </section>
