51657286890b75784c21cec8834a6a84f3dbff8c
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-12-02 17:21:00 +0000

    Update cb-utility.php

--- inc/cb-utility.php
+++ inc/cb-utility.php
@@ -133,7 +133,12 @@ function cb_gutenberg_admin_styles()
             /* Width of "full-wide" blocks */
             .wp-block[data-align="full"] {
                 max-width: none;
-            }	
+            }
+
+            /* stop horizontal overflow flicker */
+            .block-editor-page #wpwrap {
+                overflow-y: auto !important;
+            }
         </style>
     ';
 }
