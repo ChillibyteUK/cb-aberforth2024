78b08f607b2c1078bfa2942b87bc39393ed242cf
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-01-24 11:15:02 +0000

    Update restricted_page.php

--- page-templates/restricted_page.php
+++ page-templates/restricted_page.php
@@ -8,24 +8,24 @@
 get_header(); ?>
 
 <main id="main" class="site-main">
-
-    <?php
-    if (is_user_logged_in()) {
-        // Content for logged-in users
-        if (have_posts()) :
-            while (have_posts()) : the_post();
-                the_title('<h1 class="entry-title">', '</h1>');
-                the_content();
-            endwhile;
-        else :
-            echo '<p>No content found.</p>';
-        endif;
-    } else {
-        // Message for non-logged-in users
-        echo '<p>You must be logged in to view this page. <a href="' . wp_login_url(get_permalink()) . '">Click here to log in</a>.</p>';
-    }
-    ?>
-
+    <div class="container-xl">
+        <?php
+        if (is_user_logged_in()) {
+            // Content for logged-in users
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+                    the_title('<h1 class="entry-title">', '</h1>');
+                    the_content();
+                endwhile;
+            else :
+                echo '<p>No content found.</p>';
+            endif;
+        } else {
+            // Message for non-logged-in users
+            echo '<p>You must be logged in to view this page.</p>';
+        }
+        ?>
+    </div>
 </main><!-- #main -->
 <?php
 
