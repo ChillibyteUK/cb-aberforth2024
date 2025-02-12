6dad3613aec88df38a10f50233a95b87991e70f8
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-02-12 12:32:48 +0000

    Update cb_home_hero.php

--- page-templates/blocks/cb_home_hero.php
+++ page-templates/blocks/cb_home_hero.php
@@ -1,11 +1,11 @@
 <section class="home_hero">
     <div class="container-xl">
         <div class="row g-5">
-            <div class="col-lg-8 my-auto">
+            <div class="col-lg-8">
                 <h1 class="mb-5"><?= get_field('title') ?></h1>
                 <img src="<?= get_stylesheet_directory_uri() ?>/img/scale.svg" alt="">
             </div>
-            <div class="col-lg-4 my-auto">
+            <div class="col-lg-4">
                 <?= wp_get_attachment_image(get_field('image'), 'full') ?>
             </div>
         </div>
