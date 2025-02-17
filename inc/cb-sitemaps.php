3e272a3af9a57e0324abcbabbf60a8cecf397d2f
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-02-17 13:27:00 +0000

    Update cb-sitemaps.php

--- inc/cb-sitemaps.php
+++ inc/cb-sitemaps.php
@@ -1,11 +1,54 @@
 <?php
 
-function generate_custom_sitemap($sitemap_name, $urls) {
+// Define global sitemaps array
+global $sitemaps;
+$sitemaps = [
+    'asl' => [
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/dividends/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/documents/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/fees-charges/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/performance/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/portfolio/'
+    ],
+    'agvi' => [
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/capital-structure/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/dividends/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/documents/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/fees-charges/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/launch-information/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/performance/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/portfolio/'
+    ],
+    'asit' => [
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/dividends/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/documents/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/performance/'
+    ],
+    'uk-small' => [
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/documents/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/fees-charges/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/income/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/performance/',
+        'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/portfolio/'
+    ]
+];
+
+function generate_custom_sitemap($sitemap_name) {
+    global $sitemaps; // Access the global sitemaps array
+
+    if (!isset($sitemaps[$sitemap_name])) {
+        return;
+    }
+
     header("Content-Type: application/xml; charset=utf-8");
     echo '<?xml version="1.0" encoding="UTF-8"?>';
     echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
     
-    foreach ($urls as $url) {
+    foreach ($sitemaps[$sitemap_name] as $url) {
         echo '<url>';
         echo '<loc>' . esc_url($url) . '</loc>';
         echo '<lastmod>' . date("Y-m-d") . '</lastmod>';
@@ -20,58 +63,8 @@ function generate_custom_sitemap($sitemap_name, $urls) {
 
 add_action('init', function() {
     if (isset($_GET['custom_sitemap'])) {
-        $sitemap_name = $_GET['custom_sitemap'];
-
-        $sitemaps = [
-            'asl' => [
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/dividends/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/documents/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/fees-charges/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/performance/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/portfolio/'
-            ],
-            'agvi' => [
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/capital-structure/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/dividends/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/documents/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/fees-charges/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/launch-information/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/performance/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/portfolio/'
-            ],
-            'asit' => [
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/dividends/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/documents/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/performance/'
-            ],
-            'uk-small' => [
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/documents/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/fees-charges/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/income/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/performance/',
-                'https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/portfolio/'
-            ]
-        ];
-
-        if (array_key_exists($sitemap_name, $sitemaps)) {
-            generate_custom_sitemap($sitemap_name, $sitemaps[$sitemap_name]);
-        }
+        generate_custom_sitemap($_GET['custom_sitemap']);
     }
-});
-
-
-add_filter('wpseo_exclude_from_sitemap_by_post_ids', function($excluded_posts) {
-    $excluded_posts = [
-        get_the_ID_by_url('https://www.aberforth.co.uk/trusts-and-funds/aberforth-smaller-companies-trust-plc/'),
-        get_the_ID_by_url('https://www.aberforth.co.uk/trusts-and-funds/aberforth-geared-value-income-trust-plc/'),
-        get_the_ID_by_url('https://www.aberforth.co.uk/trusts-and-funds/aberforth-split-level-income-trust-plc/'),
-        get_the_ID_by_url('https://www.aberforth.co.uk/trusts-and-funds/aberforth-uk-small-companies-fund/')
-    ];
-    return $excluded_posts;
 });
 
 function get_the_ID_by_url($url) {
@@ -80,3 +73,17 @@ function get_the_ID_by_url($url) {
     return $post_id ? $post_id : null;
 }
 
+add_filter('wpseo_exclude_from_sitemap_by_post_ids', function($excluded_posts) {
+    global $sitemaps; // Access the global sitemaps array
+
+    foreach ($sitemaps as $sitemap => $urls) {
+        foreach ($urls as $url) {
+            $post_id = get_the_ID_by_url($url);
+            if ($post_id) {
+                $excluded_posts[] = $post_id;
+            }
+        }
+    }
+
+    return $excluded_posts;
+});
