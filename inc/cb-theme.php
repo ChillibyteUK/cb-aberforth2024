21847b4c7529bba6090d2628151290b146bc0443
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-02-26 10:30:27 +0000

    Update cb-theme.php

--- inc/cb-theme.php
+++ inc/cb-theme.php
@@ -12,7 +12,7 @@ require_once CB_THEME_DIR . '/inc/cb-sitemaps.php';
 
 // Remove unwanted SVG filter injection WP
 remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
-remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
+// remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
 
 define('CSV_HOST', 'https://ap01.chillihosting.co.uk');
 
